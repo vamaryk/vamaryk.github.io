e7696cc3a2f424974610c49648696afc41500af8
vamaryk <168415521+vamaryk@users.noreply.github.com>
2024-08-26 09:17:12 +0400

    Add files via upload

---
+++ gallery/gallery_page/js/app.js
@@ -0,0 +1,26 @@
+window.onload = function() {
+    setTimeout(function() {
+
+        document.body.classList.add('loaded')
+
+        Draggable.create('.gallery', {
+            bounds: 'body',
+        })
+
+    }, 200)
+}
+
+const toggle = document.querySelector('.toggle');
+
+toggle.addEventListener('click', e => {
+  document.documentElement.classList.toggle('dark');
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+      // инициализация слайдера
+      var slider = new SimpleAdaptiveSlider('.slider', {
+        autoplay: true,
+        interval: 5000,
+        swipe: true,
+      });
+    });
