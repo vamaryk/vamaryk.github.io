8c2e34a334b75e3779ba94689ffd801afed9914e
vamaryk <168415521+vamaryk@users.noreply.github.com>
2024-08-26 09:15:20 +0400

    Update app.js

--- gallery/js/app.js
+++ gallery/js/app.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
         document.body.classList.add('loaded')
 
-        Draggable.create('.gallery', {
+        Draggable.create('.slider', {
             bounds: 'body',
         })
 
@@ -24,37 +24,3 @@ document.addEventListener('DOMContentLoaded', function () {
         swipe: true,
       });
     });
-
-// Получаем ссылки на элементы
-const homeLink = document.getElementById('home-link');
-const galleryLink = document.getElementById('gallery-link');
-const galleryLink_2 = document.getElementById('gallery-link_2');
-const galleryLink_3 = document.getElementById('gallery-link_3');
-const homePage = document.getElementById('home-page');
-const homePage_2 = document.getElementById('home-page_2');
-const galleryPage = document.getElementById('gallery-page');
-
-// Добавляем обработчики событий
-homeLink.addEventListener('click', () => {
-  homePage.style.display = 'block';
-  homePage_2.style.display = 'block';
-  galleryPage.style.display = 'none';
-});
-
-galleryLink.addEventListener('click', () => {
-  homePage.style.display = 'none';
-  homePage_2.style.display = 'none';
-  galleryPage.style.display = 'block';
-});
-
-galleryLink_2.addEventListener('click', () => {
-  homePage.style.display = 'none';
-  homePage_2.style.display = 'none';
-  galleryPage.style.display = 'block';
-});
-
-galleryLink_3.addEventListener('click', () => {
-  homePage.style.display = 'none';
-  homePage_2.style.display = 'none';
-  galleryPage.style.display = 'block';
-});
