62ffda4f36bc2b3dfc1b0366c91f3c5c549f4f26
vamaryk <168415521+vamaryk@users.noreply.github.com>
2024-08-27 18:08:18 +0400

    Update app.js

--- gallery/js/app.js
+++ gallery/js/app.js
@@ -1,11 +1,33 @@
-window.onload = function() {
-    setTimeout(function() {
+// window.onload = function() {
+//     setTimeout(function() {
 
-        document.body.classList.add('loaded')
+//         document.body.classList.add('loaded')
 
         
 
-    }, 200)
+//     }, 200)
+// }
+
+// const toggle = document.querySelector('.toggle');
+
+// toggle.addEventListener('click', e => {
+//   document.documentElement.classList.toggle('dark');
+// });
+
+// document.addEventListener('DOMContentLoaded', function () {
+//       // инициализация слайдера
+//       var slider = new SimpleAdaptiveSlider('.slider', {
+//         autoplay: true,
+//         interval: 5000,
+//         swipe: true,
+//       });
+//     });
+
+import { getTheme, toggleTheme } from '../theme.js';
+window.onload = function() {
+  setTimeout(function() {
+    document.body.classList.add('loaded')
+  }, 200)
 }
 
 const toggle = document.querySelector('.toggle');
@@ -14,11 +36,22 @@ toggle.addEventListener('click', e => {
   document.documentElement.classList.toggle('dark');
 });
 
+import { getTheme, toggleTheme } from './theme.js';
+
+toggle.addEventListener('click', () => {
+  toggleTheme();
+});
+
 document.addEventListener('DOMContentLoaded', function () {
-      // инициализация слайдера
-      var slider = new SimpleAdaptiveSlider('.slider', {
-        autoplay: true,
-        interval: 5000,
-        swipe: true,
-      });
-    });
+  // инициализация слайдера
+  var slider = new SimpleAdaptiveSlider('.slider', {
+    autoplay: true,
+    interval: 5000,
+    swipe: true,
+  });
+});
+
+window.onload = () => {
+  const currentTheme = getTheme();
+  document.documentElement.classList.toggle('dark', currentTheme === 'dark');
+};
