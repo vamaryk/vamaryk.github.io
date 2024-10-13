9ac3167fadc18f4909393ba97b19c4e86ad14f3a
vamaryk <168415521+vamaryk@users.noreply.github.com>
2024-10-13 20:19:41 +0400

    Delete gallery/gallery_page/script404.js

--- gallery/gallery_page/script404.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector(".menu");
-const navigation = document.querySelector(".navigation");
-menu.onclick = () => {
-	menu.classList.toggle("active");
-	navigation.classList.toggle("active");
-};
